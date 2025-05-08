2f4a9bb0b3e38d0b704d1d011b47ed65e0f6d49d
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-05-08 21:02:05 +0200

    init project

---
+++ src/test/java/com/raa/restApiApp/RestApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raa.restApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
