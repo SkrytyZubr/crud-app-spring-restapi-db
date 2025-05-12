a92e35068052565bacecf08e3665917eee4b8161
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-05-13 00:37:29 +0200

    method put company added

--- src/main/java/com/raa/restApiApp/company/CompanyController.java
+++ src/main/java/com/raa/restApiApp/company/CompanyController.java
@@ -1,8 +1,8 @@
 package com.raa.restApiApp.company;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +18,13 @@ public class CompanyController {
     @GetMapping
     public List<Company> getAll(){
         return companyService.getAllCompanies();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCompany(@PathVariable Long id,
+                                                @RequestBody Company company){
+        companyService.updateCompany(company, id);
+        return new ResponseEntity<>("Company updated successfully",
+                HttpStatus.OK);
     }
 }
