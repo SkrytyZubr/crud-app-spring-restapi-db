cd4ec305c7b658904c046b67387e87c40a5e01a2
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-05-13 17:57:16 +0200

    method post review added

--- src/main/java/com/raa/restApiApp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/raa/restApiApp/review/impl/ReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.raa.restApiApp.review.impl;
 
+import com.raa.restApiApp.company.Company;
+import com.raa.restApiApp.company.CompanyService;
 import com.raa.restApiApp.review.Review;
 import com.raa.restApiApp.review.ReviewRepository;
 import com.raa.restApiApp.review.ReviewService;
@@ -9,15 +11,29 @@ import java.util.List;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
+    private final CompanyService companyService;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository,
+                             CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         List<Review> reviews = reviewRepository.findByCompanyId(companyId);
         return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if (company != null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        }
+        return false;
     }
 }
