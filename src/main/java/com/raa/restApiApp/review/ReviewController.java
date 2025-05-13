79777734d0960dd8fa3f84e0c9bb2cf5d700aeda
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-05-13 18:57:43 +0200

    method del review added

--- src/main/java/com/raa/restApiApp/review/ReviewController.java
+++ src/main/java/com/raa/restApiApp/review/ReviewController.java
@@ -48,4 +48,14 @@ public class ReviewController {
         else
             return new ResponseEntity<>("Review not updated", HttpStatus.NOT_FOUND);
     }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable Long companyId,
+                                               @PathVariable Long reviewId){
+        boolean isReviewDeleted = reviewService.deleteReview(companyId, reviewId);
+        if (isReviewDeleted)
+            return new ResponseEntity<>("Review deleted successfully", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Review not deleted", HttpStatus.NOT_FOUND);
+    }
 }
