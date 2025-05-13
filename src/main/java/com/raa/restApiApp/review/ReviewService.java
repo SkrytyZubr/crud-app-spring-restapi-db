48333313240315a3b06a062de46971e602c04372
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-05-13 15:29:01 +0200

    method get all reviews added

--- src/main/java/com/raa/restApiApp/review/ReviewService.java
+++ src/main/java/com/raa/restApiApp/review/ReviewService.java
@@ -1,4 +1,7 @@
 package com.raa.restApiApp.review;
 
+import java.util.List;
+
 public interface ReviewService {
+    List<Review> getAllReviews(Long companyId);
 }
--- src/main/java/com/raa/restApiApp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/raa/restApiApp/review/impl/ReviewServiceImpl.java
@@ -1,9 +1,23 @@
 package com.raa.restApiApp.review.impl;
 
+import com.raa.restApiApp.review.Review;
 import com.raa.restApiApp.review.ReviewRepository;
 import com.raa.restApiApp.review.ReviewService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewServiceImpl implements ReviewService {
+    private ReviewRepository reviewRepository;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews;
+    }
 }
